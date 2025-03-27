eda763d0948e20c64c88a6d90fcb46c4c6539834
muskanqed <muskanqed@gmail.com>
2025-03-27 07:16:04 +0530

    Added a arc to the canvas

---
+++ track.js
@@ -0,0 +1,17 @@
+class Track {
+    constructor(center, radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+    draw(ctx) {
+        // Starts a new drawing path so that the circle can be drawn without affecting other drawings.
+        ctx.beginPath();
+        //  This will draw a full circle in arc method we use radius to create a full 360 decree circle
+        // Refer the CircleCreation image for better understanding
+        // ctx.arc(x, y, radius, startAngle, endAngle);
+        ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
+        ctx.strokeStyle = "white";
+        ctx.stroke();
+    }
+}
